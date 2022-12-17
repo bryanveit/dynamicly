d6c869911f4051a6361b4df046d6f9c1b65e9204
Bryan Veit <>
2022-12-17 15:30:09 -0800

    requirements.txt update.

--- dynamicly/signal/audio.py
+++ dynamicly/signal/audio.py
@@ -1,9 +1,12 @@
 import soundfile as sf
 import sounddevice as sd
 from dynamicly.signal.sample_rate import calc_sample_rate
+from dynamicly.misc.math import normalize
 
-def play_audio(data, loop = False):
-    fs = int(calc_sample_rate(data))
-    sd.play(data[:,-1],fs,loop=loop)
+
+def play_audio(time_history, loop=False, norm=False):
+    fs = int(calc_sample_rate(time_history))
+    if norm:
+        time_history = normalize(time_history)
+    sd.play(time_history[:, -1], fs, loop=loop)
     return
-
